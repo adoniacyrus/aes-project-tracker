a12935df213a27cf054c006443ebb06c635d4652
Adonia Cyrus <adoniacyrus60@gmail.com>
2026-06-19 12:39:37 +0530

    Add initial admin user seed

---
+++ database/seeds/admin-user.sql
@@ -0,0 +1,22 @@
+INSERT INTO users
+(
+first_name,
+last_name,
+email,
+password,
+role,
+designation,
+organization,
+status
+)
+VALUES
+(
+'System',
+'Admin',
+'admin@aes.com',
+'HASH_HERE',
+'admin',
+'Administrator',
+'AES',
+'active'
+);
