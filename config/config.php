1a9308c56cbb5b9e7328e91ffba43f6eb0cba8fd
Adonia Cyrus <adoniacyrus60@gmail.com>
2026-06-19 13:00:09 +0530

    Add application configuration and database connection

---
+++ config/config.php
@@ -0,0 +1,12 @@
+<?php
+
+define('APP_NAME', 'AES Project Tracker');
+
+define('BASE_URL', 'http://localhost/aes-project-tracker');
+
+define('DB_HOST', 'localhost');
+define('DB_USER', 'root');
+define('DB_PASS', '');
+define('DB_NAME', 'aes_project_tracker');
+
+define('SESSION_TIMEOUT', 1800);
